e9aa9b68472676653b53966f937ded6486ca4c19
PenderManch <51484982+PenderManch@users.noreply.github.com>
2019-06-07 21:48:24 +0700

    :D
    :D

---
+++ N.py
@@ -0,0 +1,9 @@
+import random
+so = int(input("Nhập số M:"))
+m = (random.randint(0,100))
+if so = m:
+    print("hai so bang nhau")
+if so > m:
+    print("so ban nhap lon hon m")
+if so < m:
+    print("so ban nhap be hon m")
